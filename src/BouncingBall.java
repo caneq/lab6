67f0f5a3bb7d3b8fb48f346774a637ebb58898f2
Korobkin Alexander <korobkin.alex2000@yandex.ru>
2020-02-29 18:23:39 +0300

    add getters and setters for ball coordinates

--- src/BouncingBall.java
+++ src/BouncingBall.java
@@ -19,6 +19,22 @@ public class BouncingBall implements Runnable {
     private double x;
     private double y;
 
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
     // Вертикальная и горизонтальная компонента скорости
     private int speed;
     private double speedX;
